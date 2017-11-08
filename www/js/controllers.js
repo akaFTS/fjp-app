8f2dd2b13d7623d993d8ce1fa1619a32a9bbec19
akaFTS <gustavo84171@hotmail.com>
2017-11-08 13:26:17 -0200

    Dash parcialmente esquematizada

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,15 @@
 angular.module('starter.controllers', [])
 
-.controller('NewsCtrl', function($scope) {
-  
+.controller('NewsCtrl', function($scope, $ionicModal) {
+
+  $scope.openFlash = function() {
+    $ionicModal.fromTemplateUrl('templates/flashcard.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  }
 })
 
 .controller('CatsCtrl', function($scope) {

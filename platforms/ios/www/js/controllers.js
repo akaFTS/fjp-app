9b5258e02623799918dde37b85327d6f7516e0ed
akaFTS <gustavo84171@hotmail.com>
2017-11-08 19:24:52 -0200

    Platform

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,39 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope, $state) {
+  $scope.login = function() {
+    $state.go('tab.news');
+  }
+})
+
+.controller('NewsCtrl', function($scope, $ionicModal) {
+
+  $scope.openFlash = function() {
+    $ionicModal.fromTemplateUrl('templates/flashcard.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  }
+})
+
+.controller('CatsCtrl', function($scope) {
+
+})
+
+.controller('SearchCtrl', function($scope) {
+
+})
+.controller('SettingsCtrl', function($scope) {
+
+})
+.controller('SavedCtrl', function($scope) {
+
+})
+
+.controller('NewsContentCtrl', function($scope) {
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    }
+})
